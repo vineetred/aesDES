bb4003a7dd9840fc2446aa150412b002bb364b57
Vineet Reddy <hotshot2797@gmail.com>
2019-04-20 21:06:42 +0530

    Removed playground

--- Legacy/testing.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-mes = "Hello World"
-mes = mes.encode()
-hash_object = hashlib.sha256(mes)
-hex_dig = hash_object.hexdigest()
-
-file = open("hashed_message.txt", "w")
-file.write(str(hex_dig))
